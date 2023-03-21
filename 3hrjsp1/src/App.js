6e05b4e477b7f5a647a822145d054dcf1913b567
aashishkumar99info <aashishkumar99info@gmail.com>
2023-03-21 12:38:14 +0530

    first commit as basic project

--- 3hrjsp1/src/App.js
+++ 3hrjsp1/src/App.js
@@ -5,6 +5,15 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+        style={{fontFamily: 'cursive',color:"#ccccff"}}
+        >
+          "This Is A Basic Project I Create To Start My New Project And I Am Aash".
+        </a>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -15,7 +24,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React 
         </a>
       </header>
     </div>
