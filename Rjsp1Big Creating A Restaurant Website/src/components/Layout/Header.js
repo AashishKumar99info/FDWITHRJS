8f8935982238bd924b4236246f55995fec65953e
aashishkumar99info <aashishkumar99info@gmail.com>
2023-03-23 23:52:56 +0530

    First Commit Task-1 Building The Header

---
+++ Rjsp1Big Creating A Restaurant Website/src/components/Layout/Header.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Fragment } from "react";
+import "./Header.css"
+import mealsImage from "../../assets/meals.jpg"
+import HeaderCartButton from "./HeaderCartButton";
+
+
+const Header = (props) => {
+    return (
+        <Fragment>
+            <header className="header">
+                <h1>Here Is The Meal You Want</h1>
+                <HeaderCartButton />
+            </header>
+            <div className="main-image">
+                <img src = {mealsImage} alt = "A table full of delicious food!" />
+            </div>
+        </Fragment>
+    );
+}
+
+export default Header;
