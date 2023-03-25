22809b5b88206bd2bc508889432a087327f93b95
aashishkumar99info <aashishkumar99info@gmail.com>
2023-03-25 09:34:20 +0530

    Task-3 Making the Items visible

---
+++ Rjsp1Big Creating A Restaurant Website/src/components/Meals/MealItem/MealItem.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./MealIteam.css"
+
+const MealIteam = props => {
+    const price = `$${props.price.toFixed(2)}`;
+    
+    return (
+        <li className="meal">
+            <div>
+                <h3> {props.name} </h3>
+                <div className="description"> {props.description} </div>
+                <div className="price"> {price} </div>
+            </div>
+            <div></div>
+        </li>
+    );
+};
+
+export default MealIteam;
