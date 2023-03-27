4c9b654e64424b66d67311e213d9463dfe8076d2
aashishkumar99info <aashishkumar99info@gmail.com>
2023-03-27 17:59:04 +0530

    Task-8 Showing and Enabling the delete of Cart Items

--- Rjsp1Big Creating A Restaurant Website/src/components/Cart/Cart.js
+++ Rjsp1Big Creating A Restaurant Website/src/components/Cart/Cart.js
@@ -1,13 +1,36 @@
+import { useContext } from "react";
 import React from "react";
-import "./Cart.css"
+import CartItem from "./CartItem";
+import "./Cart.css";
 import Modal from "../UI/Modal";
+import CartContext from "../../store/cart-context";
 
 const Cart = (props) => {
+  const cartCtx = useContext(CartContext);
+
+  const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
+  const hasItems = cartCtx.items.length > 0;
+
+  const cartItemRemoveHandler = (id) => {
+    cartCtx.removeItem(id);
+  };
+
+  const cartItemAddHandler = (item) => {
+    cartCtx.addItem({...item,amount:1});
+  };
+
   const cartItems = (
-    <ul className="cart-items">
-      {[{ di: "c1", name: "Sushi", amount: "2", price: "12.99" }].map((item) => (
-          <li>{item.name}</li>
-        ))}
+    <ul className="cart-items"> 
+      {cartCtx.items.map((item) => (
+        <CartItem
+          key={item.id}
+          name={item.name}
+          amount={item.amount}
+          price={item.price}
+          onRemove={cartItemRemoveHandler.bind(null, item.id)}
+          onAdd={cartItemAddHandler.bind(null, item)}
+        />
+      ))}
     </ul>
   );
 
@@ -16,12 +39,13 @@ const Cart = (props) => {
       {cartItems}
       <div className="total">
         <span>Total Amount</span>
-        <span>35.62</span>
+        <span>{totalAmount}</span>
       </div>
       <div className="actions">
         <button className="button--alt" onClick={props.onClose}>
-          Close</button>
-        <button className="button">Order</button>
+          Close
+        </button>
+        {hasItems && <button className="button">Order</button>}
       </div>
     </Modal>
   );
